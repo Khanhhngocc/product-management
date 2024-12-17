8e8a05dc7b4498d75c496d26de70674dd9899bde
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-17 17:38:35 +0700

    khai báo dùng file tĩnh, sửa 1 số lỗi

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config;
+require("dotenv").config();
 
 const route = require("./routes/client/index.route");
 
@@ -7,8 +7,10 @@ const route = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
 
-app.set("view", "./views");
+app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 //Routes
 route(app);
