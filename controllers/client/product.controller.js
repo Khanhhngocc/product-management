6a7a5cdda11cb54ab6aae7612a1dec0401207ae2
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-17 19:51:18 +0700

    Làm trang danh sách sản phẩm bên Client

---
+++ controllers/client/product.controller.js
@@ -0,0 +1,21 @@
+const Product = require("../../models/product.model");
+
+module.exports.index = async(req, res) => {
+    const find = {
+        deleted: false, 
+        status: "active"
+    }
+
+    const products = await Product.find(find);
+
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+        return item;
+    });
+
+    res.render("client/pages/products/index", {
+        pageTitle: "Danh sách sản phẩm",
+        products: newProducts
+    })
+
+}
