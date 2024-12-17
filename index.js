fcabde2df63616b15c80bd171d69975fa5229f82
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-17 17:05:11 +0700

    set environments for Views

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 require("dotenv").config;
 
+const route = require("./routes/client/index.route");
+
 
 const app = express();
 const port = process.env.PORT;
+
+app.set("view", "./views");
+app.set("view engine", "pug");
+
+//Routes
+route(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
