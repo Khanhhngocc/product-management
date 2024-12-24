591108c87c5b1d82ba108fc56bdd568979c29675
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-24 18:39:02 +0700

    Tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -28,18 +28,28 @@ module.exports.index = async(req, res) => {
         filterStatus[index].class = "active";
     }
 
-    const find = {
+    let find = {
         deleted: false,
     }
     
     if(req.query.status){
         find.status = req.query.status;
     }
+
+    let keyword = "";
+    if(req.query.keyword){
+        keyword = req.query.keyword;
+
+        const regex = new RegExp(keyword, "i");
+        find.title = regex;
+    }
+
     const products = await Product.find(find);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     }); 
 }
