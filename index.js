92ecd1d9ab74cae5ef43051c233f1c06be3ab36e
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-25 15:55:16 +0700

    Tính năng thay đổi trạng thái sản phẩm

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const methodOverride = require('method-override');
 require("dotenv").config();
 
 const database = require("./config/database");
@@ -10,9 +11,10 @@ const routeAdmin = require("./routes/admin/index.route");
 
 database.connect();
 
-
 const app = express();
 const port = process.env.PORT;
+
+app.use(methodOverride('_method'));;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
