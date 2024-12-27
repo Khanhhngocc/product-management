bd3d7eb5b26bac705acc6f3d190c8dde5e043391
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-28 00:00:14 +0700

    Thay đổi trạng thái nhiều sản phẩm (Many)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -67,6 +67,25 @@ module.exports.deleteItem = async(req, res) => {
     });
 
     res.redirect("back");
+}
 
+//[PATCH] /admin/change-multi
+module.exports.changeMulti = async(req, res) => {
+    const type = req.body.type;
+    const ids = req.body.ids.split(", ");
 
+    switch (type) {
+        case "active":
+            await Product.updateMany({_id: { $in: ids }}, { status: "active"});
+            break;
+
+        case "inactive":
+            await Product.updateMany({_id: { $in: ids}}, { status: "inactive"});
+            break;
+    
+        default:
+            break;
+    }
+
+    res.redirect("back");
 }
