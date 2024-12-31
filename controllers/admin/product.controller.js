d78f3d0ec31f70821987bd38bfe99115d0b5b3f5
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-31 22:23:42 +0700

    19.1 Validate dữ liệu

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -133,7 +133,6 @@ module.exports.create = async(req, res) => {
 }
 
 module.exports.createPost = async(req, res) => {
-    console.log(req.file);
     req.body.price = parseInt(req.body.price);
     req.body.discountPercentage = parseInt(req.body.discountPercentage);
     req.body.stock = parseInt(req.body.stock);
@@ -145,7 +144,9 @@ module.exports.createPost = async(req, res) => {
         req.body.position = parseInt(req.body.position);
     };
 
-    req.body.thumbnail = `/uploads/${req.file.filename}`;
+    if(req.file){
+        req.body.thumbnail = `/uploads/${req.file.filename}`; 
+    }
 
     const product = new Product(req.body);
     await product.save();
