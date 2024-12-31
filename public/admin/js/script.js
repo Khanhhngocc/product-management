b00c839a35d6eeeb9840a59a03432d112d7b459d
Khanhhngocc <ngoctaehyung0630@gmail.com>
2024-12-31 21:59:06 +0700

    Preview Picture

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -150,5 +150,20 @@ if(showAlert){
         showAlert.classList.add("alert-hidden");
     });
 }
-
 // End Show Alert
+
+//Up Load Image
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage){
+    const uploadImageInput = document.querySelector("[upload-image-input]");
+    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        if(file){
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+};
+
+//End Up Load Image
