7a7823c94ab6d391a26c6e9b1539896aad71b2e3
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-01-08 11:49:56 +0700

    21.1 Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -6,20 +6,61 @@ const systemConfig = require("../../config/system");
 module.exports.index = async(req, res) => {
     let find = {
         deleted: false,
+    };
+
+    function createTree(arr, parentId = ""){
+        const tree = [];
+        arr.forEach((item) => {
+            if(item.parent_id === parentId){
+                const newItem = item;
+                const children = createTree(arr, item.id);
+                if(children.length > 0){
+                    newItem.children = children;
+                }
+                tree.push(newItem);
+            }
+        });
+        return tree;
     }
 
     const records = await ProductCategory.find(find);
 
+    const newRecords = createTree(records);
+
     res.render("admin/pages/products-category/index.pug", {
         pageTitle: "Danh mục sản phẩm",
-        records: records
+        records: newRecords
     });
 };
 
 //[GET] /admin/products-category/create
-module.exports.create = (req, res) => {
+module.exports.create = async(req, res) => {
+    let find = {
+        deleted: false,
+    };
+
+    function createTree(arr, parentId = ""){
+        const tree = [];
+        arr.forEach((item) => {
+            if(item.parent_id === parentId){
+                const newItem = item;
+                const children = createTree(arr, item.id);
+                if(children.length > 0){
+                    newItem.children = children;
+                }
+                tree.push(newItem);
+            }
+        });
+        return tree;
+    }
+
+    const records = await ProductCategory.find(find);
+
+    const newRecords = createTree(records);
+
     res.render("admin/pages/products-category/create.pug", {
         pageTitle: "",
+        records: newRecords
     });
 };
 
