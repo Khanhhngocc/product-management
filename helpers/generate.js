6dd05946c538dc3ad42b146ae0f6f5f957a31eba
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-01-11 16:51:48 +0700

    22.1.2 + 1.3 + 1.4

--- helpers/generate.js
+++ helpers/generate.js
@@ -1,4 +1,4 @@
-module.exports.generateRandomString = () => {
+module.exports.generateRandomString = (length) => {
     const characters = 
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
