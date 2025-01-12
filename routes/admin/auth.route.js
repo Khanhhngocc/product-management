c7ac7f0fb31f71afc39526834db85509fc84fb8e
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-01-12 16:45:54 +0700

    3. Làm tính năng đăng xuất

--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -8,6 +8,9 @@ router.get("/login", controller.login);
 router.post(
     "/login", 
     validate.loginPost,
-    controller.loginPost);
+    controller.loginPost
+);
+
+router.get("/logout", controller.logout);
 
 module.exports = router;
