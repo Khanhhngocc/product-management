60b0aa8cf7e08731a8bac8db680a8b2d233fc6c5
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-01-21 00:26:37 +0700

    24.4.1. Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,21 @@
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helpers/product");
+
 //[GET] /products
 module.exports.index = async(req, res) => {
+
+    //Lấy ra danh sách sản phẩm nổi bật
+    const productsFeatured = await Product.find({
+        featured: "1",
+        deleted: false,
+        status: "active"
+    })
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
+
     
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        productsFeatured: newProductsFeatured,
     })
 }
