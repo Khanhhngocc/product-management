e61d9ff8f15130ac85bb08e0e68d7aaaa544c228
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-01-23 00:39:51 +0700

    2.2. Đăng ký tài khoản

---
+++ validates/client/user.validate.js
@@ -0,0 +1,21 @@
+module.exports.registerPost = (req, res, next) => {
+    if(!req.body.fullName) {
+        req.flash("error", "Không để trống họ tên!");
+        res.redirect("back");
+        return;
+    }
+
+    if(!req.body.email) {
+        req.flash("error", "Không để trống email!");
+        res.redirect("back");
+        return;
+    }
+
+    if(!req.body.password) {
+        req.flash("error", "Không để trống mật khẩu!");
+        res.redirect("back");
+        return;
+    }
+
+    next();
+}
