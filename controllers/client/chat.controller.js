bc6d617f96923d90df506569ec923a7928d72946
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-02-13 19:34:04 +0700

    28.3. Làm giao diện chat cho Project

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,6 +1,13 @@
+//[GET] /chat/
+module.exports.chat = (req, res) => {
     // SocketIO
     _io.on('connection', (socket) => {
         console.log('a user connected', socket.id);
     });
 
     // End SocketIO
+
+    res.render("client/pages/chat/index", {
+        pageTitle: "Chat"
+    })
+}
