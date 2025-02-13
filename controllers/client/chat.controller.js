a693377223e8a1b0a12260707bedeb19bcb8d40a
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-02-13 21:19:22 +0700

    5.2. Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,13 +1,44 @@
-//[GET] /chat/
-module.exports.chat = (req, res) => {
-    // SocketIO
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
-    });
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 
+//[GET] /chat/
+module.exports.chat = async(req, res) => {
+    const userId = res.locals.user.id;
+
+    // SocketIO
+    _io.once('connection', (socket) => { // on mỗi lần load lại trang là 1 lần kết nối mới -> khi gửi tin nhắn đi sẽ bị gửi nhiều lần
+        socket.on("CLIENT_SEND_MESSAGE", async(content) => {
+            // Lưu vào database
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            });
+            await chat.save();
+        });
+    });
     // End SocketIO
 
+    // Lấy ra data
+    const chats = await Chat.find({
+        deleted: false
+    });
+
+    console.log(chats);
+
+    for (const chat of chats) {
+        const infoUser = await User.findOne({
+            _id: chat.user_id,
+        }).select("fullName");
+
+        console.log(infoUser);
+        
+        chat.infoUser = infoUser;
+
+    }
+
+
     res.render("client/pages/chat/index", {
-        pageTitle: "Chat"
+        pageTitle: "Chat",
+        chats: chats,
     })
 }
