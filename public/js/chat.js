690a252e0a29c8a729b937876d6a59d89a26c075
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-02-15 12:37:57 +0700

    29.1.1. Chèn icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIENT_SEND_MESSAGE 
 const formSendData = document.querySelector(".chat .inner-form");
 if(formSendData) {
@@ -47,3 +49,26 @@ if(bodyChat) {
     bodyChat.scrollTop = bodyChat.scrollHeight;  // scroll cách Top bằng chiều cao của scroll Height
 }
 // End Scroll Chat to Bottom
+
+//Emoji-Picker
+// Show pop-up
+const buttonIcon = document.querySelector(".button-icon");
+if(buttonIcon) {
+    const tooltip = document.querySelector(".tooltip");
+    Popper.createPopper(buttonIcon, tooltip);
+
+    buttonIcon.onclick = () => {
+        tooltip.classList.toggle("shown");
+    };
+};
+
+// Insert icon to Input
+const emojiPicker = document.querySelector("emoji-picker");
+if(emojiPicker) {
+    const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+    emojiPicker.addEventListener("emoji-click", (e) => {
+        const icon = e.detail.unicode;
+        inputChat.value += icon;
+    })
+}
+//End Emoji-Picker
